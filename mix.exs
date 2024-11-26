201de79f9a009fee424820845e88995d9676d5b7
Juan Morais <timrodz@icloud.com>
2024-11-27 00:06:08 +1300

    chore: Version bump to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RacingLeaderboards.MixProject do
   def project do
     [
       app: :racing_leaderboards,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
