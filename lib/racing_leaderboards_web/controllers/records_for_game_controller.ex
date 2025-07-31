5e03142fe0f00d4d607c281dcfbe5975ba2ac8ae
Juan Morais <timrodz@icloud.com>
2025-07-31 23:39:05 +1200

    refactor: Calculate grouped_records in weekly_stats

--- lib/racing_leaderboards_web/controllers/records_for_game_controller.ex
+++ lib/racing_leaderboards_web/controllers/records_for_game_controller.ex
@@ -41,20 +41,22 @@ defmodule RacingLeaderboardsWeb.RecordsForGameController do
       end
 
     game = Games.get_game_by_code!(game_code)
-    
+
     combinations = Records.get_fastest_records_by_game_week_combinations(game.id, date)
-    
+
     processed_combinations =
       combinations
       |> Enum.map(fn {circuit, car, fastest_record, all_records} ->
         {circuit, car, fastest_record, process_records(all_records)}
       end)
 
+    grouped_records = process_weekly_combinations(processed_combinations)
+
     render(conn, "weekly_stats.html", %{
       page_title: "Weekly Stats for #{game.name} - Week of #{DateUtils.parse(parsed_date)}",
       game: game,
       date: parsed_date,
-      combinations: processed_combinations
+      grouped_records: grouped_records
     })
   end
 
@@ -147,5 +149,34 @@ defmodule RacingLeaderboardsWeb.RecordsForGameController do
         diff_time = Time.from_iso8601!("00:00:00.000") |> Time.add(diff, :millisecond)
         record |> Map.put(:diff_time, diff_time)
     end
+  end
+
+  # Process combinations for weekly stats view
+  defp process_weekly_combinations(combinations) do
+    combinations
+    |> Enum.filter(fn {_circuit, _car, _fastest_record, grouped_records} ->
+      grouped_records != nil and length(grouped_records) > 0
+    end)
+    |> Enum.map(fn {circuit, car, _fastest_record, grouped_records} ->
+      all_records =
+        grouped_records
+        |> Enum.flat_map(fn {{^circuit, ^car}, records} -> records end)
+
+      # Separate finished records from DNFs
+      finished_records =
+        all_records
+        |> Enum.filter(fn record -> not record.is_dnf end)
+        |> Enum.sort_by(fn record -> record.time end)
+
+      dnf_records =
+        all_records
+        |> Enum.filter(fn record -> record.is_dnf end)
+        |> Enum.sort_by(fn record -> record.time end)
+
+      {circuit, car, finished_records, dnf_records}
+    end)
+    |> Enum.filter(fn {_circuit, _car, finished_records, dnf_records} ->
+      length(finished_records) > 0 or length(dnf_records) > 0
+    end)
   end
 end
